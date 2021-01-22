58d6fa74b6cb56f782ef3cc8f0723882fb800270
darot-chen <chendarot1@gmail.com>
2021-01-22 15:41:25 +0700

    initail commit

---
+++ lib/main.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: Text(""),
+      appBar: AppBar(
+        title: Text("Feed Reader"),
+        actions: [
+          IconButton(icon: Icon(Icons.refresh), onPressed: () {}),
+          IconButton(icon: Icon(Icons.more_vert), onPressed: () {}),
+        ],
+      ),
+      body: ListView(
+        // padding: EdgeInsetsGeometry(),
+        children: [
+          ItemList(
+            title: "Day reappeared. The tempest still raged with undiminished",
+            subtitle: "Concern Beef procuitto ground",
+          ),
+          Divider(),
+          ItemList(
+            title: "There were some sing of calm at noon",
+            subtitle: "Things to enjoy",
+          ),
+          Divider(),
+          ItemList(
+            title: "Fun tropical escapes",
+            subtitle:
+                "The night was comparatively quiet. Some of the sails were again.",
+          ),
+          Divider(),
+          ItemList(
+            title: "Pork lion suasage shankle, keilbasa bacon beef ribs",
+            subtitle: "Drumstick turkey shoulder spare",
+          ),
+          Divider(),
+          ItemList(
+            title: "Cherry blossom in bloom",
+            subtitle: "Spring is here and we know all know ",
+          ),
+          Divider(),
+        ],
+      ),
+    );
+  }
+}
+
+class ItemList extends StatelessWidget {
+  const ItemList({
+    this.subtitle,
+    this.title,
+    Key key,
+  }) : super(key: key);
+
+  final String title;
+  final String subtitle;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // padding: EdgeInsets.all(10),
+      child: ListTile(
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              title,
+              maxLines: 2,
+              overflow: TextOverflow.fade,
+            ),
+            SizedBox(height: 10),
+            Text(
+              subtitle,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ],
+        ),
+        subtitle: Text("10 min"),
+        trailing: Container(
+          child: Image(
+            image: NetworkImage(
+                "https://cdn.britannica.com/08/187508-050-D6FB5173/Shanghai-Tower-Gensler-San-Francisco-world-Oriental-2015.jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+}
